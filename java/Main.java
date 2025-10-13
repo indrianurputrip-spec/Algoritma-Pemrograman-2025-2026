2394299b6a7406948d993a89cf700b80896df0b3
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-10-13 19:18:58 +0700

    tugas1 java

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]  args) {
+String nama = "indria nurputri p";
+int umur = 18;
+float tinggi = 168;
+boolean aktif = true;
+
+System.out.println("Nama: " + nama);
+System.out.println("Umur: " + umur);
+System.out.println("Tinggi: " + tinggi);
+System.out.println("Status Aktif: " + aktif);
+    }
+}
