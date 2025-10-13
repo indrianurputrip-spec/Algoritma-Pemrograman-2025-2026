26d657a8b48eea71e560f487e7cfe21b58edd333
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-10-13 18:03:38 +0700

    tugas1 php

---
+++ php/tugas2.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "indria nurputri p";
+$umur = 18;
+$tinggi = 168;
+$aktif = true;
+
+echo "nama: $nama<br>";
+echo "umur: $umur<br>";
+echo "tinggi: $tinggi<br>";
+echo "status aktif: $aktif<br>";
+?>
