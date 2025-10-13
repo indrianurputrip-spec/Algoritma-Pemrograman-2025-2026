c840a1c0f1f3a816429b0a6fee88e5f833760efe
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-10-13 10:48:39 +0700

    latihan C++

---
+++ c++/latihan1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama ="indria";
+    int umur = 18;
+    float tinggi = 168;
+    bool aktif = true;
+
+
+    cout <<"nama:" << nama << endl;
+    cout <<"umur:" <<umur << endl;
+    cout <<"tinggi:" <<tinggi << endl;
+    cout <<"status aktif:" <<aktif << endl;
+    return 0;
+}
