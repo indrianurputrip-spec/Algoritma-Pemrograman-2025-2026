4a36bb3bd937f7685fca656ebf489fa93303226f
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-10-13 19:32:38 +0700

    tugas2 java

---
+++ java/tugas2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class tugas2 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukan jari-jarai: ");
+        double r = sc.nextDouble();
+        double luas = PI * r * r;
+        System.out.println("Luas lingkaran: " + luas);
+    }
+}
+
+
+
+
+    
+
