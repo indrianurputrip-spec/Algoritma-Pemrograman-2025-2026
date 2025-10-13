50e94f5ad3c7cd626faab0f9e17ec2e12dd2298c
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-10-13 17:40:56 +0700

    tugas 2 c++

---
+++ c++/tugas2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama ="indria nurputri p";
+    int umur = 18;
+    float tinggi = 168;
+    bool aktif = true;
+
+    cout <<"Nama: "<<nama<<endl;
+    cout <<"Umir: "<<umur<<endl;
+    cout <<"Tinggi: "<<tinggi<<endl;
+    cout <<"Status Aktif: "<<aktif<<endl;
+    return 0;
+}
