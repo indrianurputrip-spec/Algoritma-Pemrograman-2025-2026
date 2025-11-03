37aaa5804e03a94d5155fdcc1b064e5fe3436786
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-03 19:50:37 +0700

    java

---
+++ java/tipedata.java
@@ -0,0 +1,13 @@
+public class tipedata {
+    public static void main(String[] args) {
+        String nama = "indria nurputri p";
+        int umur = 18;
+        float tinggi = 170.0f;
+        boolean aktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Aktif: " + aktif);
+    }
+}
