af501e8ed4cc5f83c6e2c77130f2a541c1b1dd00
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-03 19:47:16 +0700

    php

---
+++ php/tipedata.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "indria nurputri p";
+$umur = 18;
+$tinggi = 170;
+$aktif = true;
+
+echo "Nama: $nama<br>";
+echo "Umur: $umur<br>";
+echo "Tinggi: $tinggi<br>";
+echo "Status aktif: $aktif<br>";
+?>
