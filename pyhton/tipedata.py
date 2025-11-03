2890dd48d0d5528ad673256ec566391d8b0ffc1c
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-03 19:38:50 +0700

    pyhton

---
+++ pyhton/tipedata.py
@@ -0,0 +1,14 @@
+# /c:/Users/Denide/Documents/GitHub/Algoritma-Pemrograman-2025-2026/pyhton/tipedata.py
+def main():
+    nama = "indria nurputri p"
+    umur = 18
+    tinggi = 170.0
+    aktif = True
+
+    print("Nama:", nama)
+    print("Umur:", umur)
+    print("Tinggi:", tinggi)
+    print("Aktif:", aktif)
+
+if __name__ == "__main__":
+    main()
