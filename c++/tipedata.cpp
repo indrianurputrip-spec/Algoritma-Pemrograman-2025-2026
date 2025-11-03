05ced0348278758ff84ff0f15d1c545e5a8d6961
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-03 19:24:37 +0700

    c++

---
+++ c++/tipedata.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama = "indria nurputri p";
+    int umur = 19;
+    float tinggi = 170;
+    bool aktif = true;
+
+    cout << "NAMA: " << nama << endl;
+    cout << "UMUR: " << umur << endl;
+    cout << "TINGGI: " << tinggi << endl;
+    cout << "AKTIF: " << aktif << endl; 
+    return 0;
+}
