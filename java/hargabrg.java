5ea412eaafaf67efb9259c0b4afac0fe19de4f94
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-24 10:42:14 +0700

    java

---
+++ java/hargabrg.java
@@ -0,0 +1,15 @@
+public class hargabrg {
+    public static void main(String[] args) {
+        String namabarang = "baju";
+        int JumlahBarang = 2;
+        double HargaSatuan = 20000;
+        double TotalHarga = JumlahBarang * HargaSatuan;
+        
+        System.out.println("Nama Barang: " + namabarang);
+        System.out.println("jumlah barang: " + JumlahBarang);
+        System.out.println("Harga Satuan: " + HargaSatuan);
+        System.out.println("Total Harga: " + TotalHarga);
+
+    }
+    
+}
